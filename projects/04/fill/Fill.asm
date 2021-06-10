05712192d2675f64a59b957f7a5cceae3b1c5bf7
Heidi Tran <HeidiTran1410@gmail.com>
2021-06-10 13:09:27 -0400

    Implement fill the screen if keyboard is pressed

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,106 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// Pseudo code
+//  while (true) {
+//  	if (keyboard == 0) {
+//  		set color to draw to white
+//  	} else {
+//  		set color to draw to black
+//  	}
+//  	
+//  	// Draw the whole screen 
+//  	addr = SCREEN
+//  	row = 256
+//  	while row > 0:
+//  		row -=1
+//  		col = 32
+//  		while col > 0:
+//  			RAM[addr] = color	// Draw with color = color 
+//  			addr = addr + 1
+//  			col -= 1
+//  }
+//  
+//  MAINLOOP:
+//  	if key NOT pressed goto WHITE
+//  	color = -1
+//  	goto DRAW
+//  	
+//  WHITE:
+//  	color = 0
+//  
+//  DRAW:
+//  	addr = SCREEN
+//  	row = 256	
+//  	DRAWLOOP:
+//  		if row == 0 goto MAINLOOP
+//  		row -=1
+//  		col = 32
+//  		
+//  		ROWLOOP:
+//  			if col == 0 goto DRAWLOOP
+//  			RAM[addr] = color 
+//  			addr = addr + 1
+//  			col -= 1
+//  			goto ROWLOOP
+
+(MAINLOOP)
+   @KBD
+   D = M 
+   @WHITE 
+   D;JEQ	// if key NOT pressed goto WHITE
+   
+   @color
+   M = -1	// set color to black 
+   @DRAW
+   0;JMP 
+
+(WHITE)
+   @color
+   M = 0	// set color to white 
+
+// Draw the whole screen 
+(DRAW)   
+   @SCREEN
+   D = A 
+   @addr 
+   M = D	// addr = SCREEN
+   
+   @256
+   D = A
+   @row 
+   M = D 	// row = 256
+   
+(DRAWLOOP)
+   @row 
+   D = M 
+   @MAINLOOP
+   D;JEQ	// if row == 0 goto MAINLOOP
+   
+   @row 
+   M = M - 1	// row -= 1
+   @32
+   D = A
+   @col 
+   M = D 		// col = 32
+   
+(ROWLOOP)
+   @col 
+   D = M 
+   @DRAWLOOP
+   D;JEQ 	// if col == 0 goto DRAWLOOP  
+   
+   @color 
+   D = M 
+   @addr 
+   A = M
+   M = D 	// RAM[addr] = color 
+   
+   @addr
+   M = M + 1	// addr = addr + 1
+   
+   @col 
+   M = M - 1	// col -= 1
+   
+   @ROWLOOP
+   0;JMP 
