e2df5aa750b21b36153b78275f07ed465fe2fe06
Heidi Tran <HeidiTran1410@gmail.com>
2022-04-07 13:21:13 -0400

    Implement tokenizer

--- projects/10/JackAnalyzer/JackAnalyzer.cs
+++ projects/10/JackAnalyzer/JackAnalyzer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static JackAnalyzer.JackTokenizer;
 
 namespace JackAnalyzer
 {
@@ -14,6 +15,26 @@ namespace JackAnalyzer
 			- Creates an output file name fileName.xml
 			- Creates and uses a CompilationEngine to compile the input JackTokenizer into the output file
 			 */
+
+			// JackAnalyzer.exe Main.jack
+			// string inputPath = args[0];
+
+			// For debugging in VisualStudio
+			//string inputPath = @"C:\Projects\nand-to-tetris\projects\10\ArrayTest\Main.jack";
+			//string inputPath = @"C:\Projects\nand-to-tetris\projects\10\Square\Main.jack";
+			//string inputPath = @"C:\Projects\nand-to-tetris\projects\10\Square\Square.jack";
+			string inputPath = @"C:\Projects\nand-to-tetris\projects\10\Square\SquareGame.jack";
+			//string inputPath = @"C:\Projects\nand-to-tetris\projects\10\ExpressionLessSquare\test.jack";
+			JackTokenizer tokenizer = new(inputPath);
+
+			Console.WriteLine("<tokens>");
+			while (tokenizer.HasMoreTokens())
+			{
+				tokenizer.Advance();
+				string tokenTypeStr = TokenTypeStr[tokenizer.GetTokenType()];
+				Console.WriteLine("<" + tokenTypeStr + "> " + tokenizer.GetCurrentToken() + " </" + tokenTypeStr + ">");
+			}
+			Console.WriteLine("</tokens>");
 		}
 	}
 }
--- projects/10/JackAnalyzer/JackTokenizer.cs
+++ projects/10/JackAnalyzer/JackTokenizer.cs
@@ -48,6 +48,43 @@ namespace JackAnalyzer
 			THIS
 		}
 
+		private static readonly HashSet<string> _symbols = new() { 
+			"{", "}", "(", ")", "[", "]", ".", ",", ";", "+", "-", "*", "/", "&", "|", "<", ">", "=", "~" 
+		};
+		private static readonly HashSet<string> _keywords = new()
+		{
+			"class",
+			"method",
+			"function",
+			"constructor",
+			"int",
+			"boolean",
+			"char",
+			"void",
+			"var",
+			"static",
+			"field",
+			"let",
+			"do",
+			"if",
+			"else",
+			"while",
+			"return",
+			"true",
+			"false",
+			"null",
+			"this"
+		};
+		public static readonly Dictionary<TokenType, string> TokenTypeStr = new()
+		{
+			{ TokenType.KEYWORD, "keyword" },
+			{ TokenType.SYMBOL, "symbol" },
+			{ TokenType.IDENTIFIER, "identifier" },
+			{ TokenType.INT_CONST, "integerConstant" },
+			{ TokenType.STRING_CONST, "stringConstant" }
+		};
+		private bool _isSlashButNotComment; // denotes a forward slash but not one for comment
+
 		public JackTokenizer(string jackFilePath)
 		{
 			_streamReader = new StreamReader(jackFilePath);
@@ -55,18 +92,139 @@ namespace JackAnalyzer
 
 		public bool HasMoreTokens()
 		{
-			// TODO
-			return false;
+			IgnoreWhiteSpaceAndNewLine();
+			if (_streamReader.EndOfStream)
+			{
+				return false;
+			}
+
+			int nextToken = _streamReader.Peek();
+			if (nextToken == '/')
+			{
+				_streamReader.Read();
+				nextToken = _streamReader.Peek();
+
+				if (nextToken == -1)
+				{
+					_isSlashButNotComment = true;
+					return true;
+				}
+
+				if (nextToken == '/' || nextToken == '*')
+				{
+					IgnoreComment((char) nextToken);
+					return HasMoreTokens();
+				}
+
+				_isSlashButNotComment = true;
+				return true;
+			}
+
+			return !_streamReader.EndOfStream;
 		}
 
 		/// <summary>
 		/// Gets the next token from the input
 		/// and makes it the current token.
-		/// This method should be called only if `HasMoreTokens` is true.
 		/// </summary>
 		public void Advance()
 		{
+			if (_isSlashButNotComment)
+			{
+				_currentToken = "/";
+				_isSlashButNotComment = false;
+				return;
+			}
 
+			char token = (char)_streamReader.Read();
+			if (_symbols.Contains(token.ToString()))
+			{
+				_currentToken = token.ToString();
+			}
+			else if (token == '"')
+			{
+				_currentToken = ConsumeStringConstant(token);
+			}
+			else if (short.TryParse(token.ToString(), out _))
+			{
+				_currentToken = ConsumeAllDigits(token);
+			} else
+			{
+				_currentToken = ConsumeKwOrIdentifier(token);
+			}
+		}
+
+		private void IgnoreWhiteSpaceAndNewLine()
+		{
+			int nextToken = _streamReader.Peek();
+			while (nextToken != -1 && IsIgnoredTok((char)nextToken))
+			{
+				_streamReader.Read();
+				nextToken = _streamReader.Peek();
+			}
+		}
+
+		private static bool IsIgnoredTok(char token)
+		{
+			return token == ' ' || token == '\n' || token == '\t' || token == '\r';
+		}
+
+		private void IgnoreComment(char typeOfComment)
+		{
+			if (typeOfComment == '/')
+			{
+				_streamReader.ReadLine();
+
+			}
+			else if (typeOfComment == '*')
+			{
+				_streamReader.Read(); // Skip the *
+				char token = (char)_streamReader.Read();
+				int nextToken = _streamReader.Peek();
+				while (nextToken != -1 && (token != '*' || nextToken != '/'))
+				{
+					token = (char)_streamReader.Read();
+					nextToken = _streamReader.Peek();
+				}
+				_streamReader.Read(); // Skip the /
+			}
+		}
+
+		private string ConsumeAllDigits(char firstToken)
+		{
+			List<char> res = new() { firstToken };
+			int nextToken = _streamReader.Peek();
+			while (nextToken != -1 && short.TryParse(((char)nextToken).ToString(), out _))
+			{
+				res.Add((char)_streamReader.Read());
+				nextToken = _streamReader.Peek();
+			}
+			return string.Join("", res);
+		}
+
+		private string ConsumeStringConstant(char firstToken)
+		{
+			List<char> res = new() { firstToken };
+			int nextToken = _streamReader.Peek();
+			while (nextToken != -1 && nextToken != '"')
+			{
+				res.Add((char)_streamReader.Read());
+				nextToken = _streamReader.Peek();
+			}
+			res.Add((char)_streamReader.Read());
+			return string.Join("", res);
+		}
+
+		private string ConsumeKwOrIdentifier(char firstToken)
+		{
+			List<char> res = new() { firstToken };
+			int nextToken = _streamReader.Peek();
+			while (nextToken != -1 && !IsIgnoredTok((char) nextToken) && !_symbols.Contains(((char) nextToken).ToString()))
+			{
+				res.Add((char)_streamReader.Read());
+				nextToken = _streamReader.Peek();
+			}
+			return string.Join("", res);
 		}
 
 		/// <summary>
@@ -74,7 +232,23 @@ namespace JackAnalyzer
 		/// </summary>
 		public TokenType GetTokenType()
 		{
-			// TODO
+			if (_symbols.Contains(_currentToken))
+			{
+				return TokenType.SYMBOL;
+			}
+			else if (double.TryParse(_currentToken, out double num) && num >= 0 && num <= short.MaxValue)
+			{
+				return TokenType.INT_CONST;
+			}
+			else if (_keywords.Contains(_currentToken))
+			{
+				return TokenType.KEYWORD;
+			}
+			else if (_currentToken.StartsWith("\"") && _currentToken.EndsWith("\""))
+			{
+				// TODO: Check that the inner text is a unicode char + not including double quote + not including newline
+				return TokenType.STRING_CONST;
+			}
 			return TokenType.IDENTIFIER;
 		}
 
@@ -86,7 +260,11 @@ namespace JackAnalyzer
 		/// <returns></returns>
 		public KeyWord GetKeyWord()
 		{
-			_ = Enum.TryParse<KeyWord>(_currentToken, out KeyWord keyWord);
+			if (GetTokenType() != TokenType.KEYWORD)
+			{
+				throw new Exception("GetKeyWord should be called only if `GetTokenType` is KEYWORD!");
+			}
+			_ = Enum.TryParse(_currentToken, out KeyWord keyWord);
 			return keyWord;
 		}
 
@@ -96,8 +274,11 @@ namespace JackAnalyzer
 		/// </summary>
 		public char GetSymbol()
 		{
-			// TODO
-			return 'a';
+			if (GetTokenType() != TokenType.SYMBOL)
+			{
+				throw new Exception("GetSymbol should be called only if `GetTokenType` is SYMBOL!");
+			}
+			return char.Parse(_currentToken);
 		}
 
 		/// <summary>
@@ -106,8 +287,11 @@ namespace JackAnalyzer
 		/// </summary>
 		public string GetIdentifier()
 		{
-			// TODO
-			return string.Empty;
+			if (GetTokenType() != TokenType.IDENTIFIER)
+			{
+				throw new Exception("GetIdentifier should be called only if `GetTokenType` is IDENTIFIER!");
+			}
+			return _currentToken;
 		}
 
 		/// <summary>
@@ -115,8 +299,7 @@ namespace JackAnalyzer
 		/// </summary>
 		public int GetIntVal()
 		{
-			TokenType tokenType = GetTokenType();
-			if (tokenType != TokenType.INT_CONST)
+			if (GetTokenType() != TokenType.INT_CONST)
 			{
 				throw new Exception("GetIntVal should be called only if `GetTokenType` is INT_CONST!");
 			}
@@ -129,11 +312,33 @@ namespace JackAnalyzer
 		/// </summary>
 		public string GetStringVal()
 		{
-			TokenType tokenType = GetTokenType();
-			if (tokenType != TokenType.STRING_CONST)
+			if (GetTokenType() != TokenType.STRING_CONST)
 			{
 				throw new Exception("GetStringVal should be called only if `GetTokenType` is STRING_CONST!");
 			}
+			return _currentToken[1..^1];
+		}
+
+		private static readonly Dictionary<string, string> _symbolsToXMLConvention = new()
+		{
+			{ "<", "&lt;" },
+			{ ">", "&gt;" },
+			{ "\"", "&quot;" },
+			{ "&", "&amp;" }
+		};
+
+		public string GetCurrentToken()
+		{
+			if (GetTokenType() == TokenType.STRING_CONST)
+			{
+				return _currentToken[1..^1];
+			}
+
+			if (_symbolsToXMLConvention.ContainsKey(_currentToken))
+			{
+				return _symbolsToXMLConvention[_currentToken];
+			}
+
 			return _currentToken;
 		}
 	}
